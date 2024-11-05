25f080c77fc5a1bbf3ced92f671d087180c16e5f
Salma-007 <salmaelallali11@gmail.com>
2024-11-05 15:41:14 +0100

    add the variables in js file

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+const btn_like = document.querySelector(".fill");
+const btn_dislike = document.querySelector(".outline");
+let banner_image = document.querySelector(".banner-image");
+const titre = document.getElementById("titre");
+const description = document.getElementById("description");
+
+btn_like.addEventListener("click", () => {
+    banner_image.style.backgroundImage = "none";  
+
+
+    
+});
